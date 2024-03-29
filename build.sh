403272e48f9d9fec7fc73f599a9adca6a6f02498
jrc03c <jrc03c@pm.me>
2024-03-29 11:54:30 -0500

    ...

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ YELLOW="\033[1;33m"
 RED="\033[1;31m"
 CLEAR="\033[0m"
 
-echo -e "${YELLOW}NOTE: Don't forget to update the version number in ${RED}setup.py${YELLOW} if you haven't already done so!${CLEAR}"
+echo -e "${YELLOW}NOTE: Don't forget to update the version number in ${RED}setup.py${YELLOW} if you haven't already done so! (And if you haven't already done so, then please re-run this script after updating the version number!)${CLEAR}"
--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@ from setuptools import setup
 
 setup(
     name="sentence_meaning_helper",
-    version="0.0.1",
+    version="0.0.2",
     description="Some tools for getting sentence meanings w/ Sentence-BERT",
     url="https://github.com/jrc03c/sentence_meaning_helper",
     author="jrc03c",
     author_email="jrc03c@pm.me",
     license="none",
     packages=["sentence_meaning_helper"],
-    classifiers=["Topic :: Scientific/Engineering :: Mathematics",],
+    classifiers=[
+        "Topic :: Scientific/Engineering :: Mathematics",
+    ],
     include_package_data=True,
 )
